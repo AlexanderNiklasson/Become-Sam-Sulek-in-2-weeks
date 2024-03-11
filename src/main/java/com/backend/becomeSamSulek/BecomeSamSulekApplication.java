4897d3d11ce71a1a8624b783ffc4c20236f7fa56
Alexander Niklasson <alexander.niklasson@live.com>
2024-03-11 14:24:41 +0100

    added spring

---
+++ src/main/java/com/backend/becomeSamSulek/BecomeSamSulekApplication.java
@@ -0,0 +1,13 @@
+package com.backend.becomeSamSulek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecomeSamSulekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BecomeSamSulekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/becomeSamSulek/BecomeSamSulekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.becomeSamSulek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecomeSamSulekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
