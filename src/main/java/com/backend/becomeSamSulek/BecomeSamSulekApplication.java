d73e061ae693fecfa67f16597ef8d40deccbca16
Nicolai Klokmose <angryniczorn@gmail.com>
2024-03-15 10:01:24 +0100

    testing branch: test implemented console.log in main()

--- src/main/java/com/backend/becomeSamSulek/BecomeSamSulekApplication.java
+++ src/main/java/com/backend/becomeSamSulek/BecomeSamSulekApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BecomeSamSulekApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test of branch: test");;
 		SpringApplication.run(BecomeSamSulekApplication.class, args);
 	}
 
